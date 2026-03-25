9968cceb985db1248b8bdc158b8b59272292972f
daridev <darideveloper@gmail.com>
2026-03-25 08:39:17 -0600

    fix (admin/auth): render unfold user and group

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
 from .models import Technology, Project
+from project import admin as _
 
 @admin.register(Technology)
 class TechnologyAdmin(ModelAdmin):
